0f0ce1a069342f2f2f1a3144548d46332ba536aa
Samihan Sangwai <103633066+Samihan127@users.noreply.github.com>
2023-10-06 11:19:36 +0530

    Add files via upload

--- java7.java
+++ java7.java
@@ -1,53 +1,45 @@
-//int to string
+/* There are three lines of output:
+For the first line, sum the lengths of  A and B.
+For the second line, write Yes if  A is lexicographically greater than B otherwise print No instead.
+For the third line, capitalize the first letter in both A and b and print them on a single line, separated by a space.
 
+Sample Input 0
 
+hello
+java
+Sample Output 0
+
+9
+No
+Hello Java
+
+*/
+
+import java.io.*;
 import java.util.*;
-import java.security.*;
-public class Solution {
- public static void main(String[] args) {
 
-  DoNotTerminate.forbidExit();
+public class java7 {
 
-  try {
-   Scanner in = new Scanner(System.in);
-   int n = in .nextInt();
-   in.close();
-   //String s=???; Complete this line below
-
-   //Write your code here
-   String s=String.valueOf(n);/////////////////////////////////////////////////////////////////////////////////////////
-   
-   
-
-   
-   if (n == Integer.parseInt(s)) {
-    System.out.println("Good job");
-   } else {
-    System.out.println("Wrong answer.");
-   }
-  } catch (DoNotTerminate.ExitTrappedException e) {
-   System.out.println("Unsuccessful Termination!!");
-  }
- }
-}
-
-//The following class will prevent you from terminating the code using exit(0)!
-class DoNotTerminate {
-
- public static class ExitTrappedException extends SecurityException {
-
-  private static final long serialVersionUID = 1;
- }
-
- public static void forbidExit() {
-  final SecurityManager securityManager = new SecurityManager() {
-   @Override
-   public void checkPermission(Permission permission) {
-    if (permission.getName().contains("exitVM")) {
-     throw new ExitTrappedException();
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int len=A.length()+B.length();
+        System.out.println(len);
+        
+        if(A.compareTo(B) > 0)
+        {
+        System.out.println("Yes");
+        
+    }   else 
+       {
+        System.out.println("No");
+       }
+        System.out.println(A.substring(0, 1).toUpperCase()+ A.substring(1) + " " + B.substring(0,1).toUpperCase()+ B.substring(1));
+        
+        
     }
-   }
-  };
-  System.setSecurityManager(securityManager);
- }
 }
+
